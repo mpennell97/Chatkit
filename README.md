b17486f5b006e5de6deac19e2acc2c29bff4be72
mpennell97 <44216252+mpennell97@users.noreply.github.com>
2019-05-29 12:54:14 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Finished
+
+Quick start:
+
+```
+$ npm install
+$ npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ npm run watch
+```
---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b07a76d0-ca03-4665-9003-b988dbee7618/token";
+const instanceLocator = "v1:us1:b07a76d0-ca03-4665-9003-b988dbee7618";
+
+export { tokenUrl, instanceLocator }
